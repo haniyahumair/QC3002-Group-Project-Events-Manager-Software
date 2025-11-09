4bad9c660c9893a74cd375cd62bca552db3a9762
ak-939 <158162012+ak-939@users.noreply.github.com>
2025-11-10 00:21:47 +0300

    Create home.js
    Signed-off-by: ak-939 <158162012+ak-939@users.noreply.github.com>

---
+++ home.js
@@ -0,0 +1,13 @@
+// imports
+import {getAuth, clearAuth, requireAuth} from './js/auth.js';
+
+// constants
+const btnLogin = document.querySelector('#btnLogin');
+const navCreate = document.querySelector('#navCreate');
+const heroCreate = document.querySelector('#heroCreate');
+const heroBrowse = document.querySelector('#heroBrowse');
+
+// main function for loading page
+if (auth.isAuthenticated) {
+  btnLogin.textContent = 'Logout';
+  
