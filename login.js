103d1cbfb61b4be4574962f2592f0167c9843555
ak-939 <158162012+ak-939@users.noreply.github.com>
2025-11-13 15:10:11 +0300

    Create login.js
    Signed-off-by: ak-939 <158162012+ak-939@users.noreply.github.com>

---
+++ login.js
@@ -0,0 +1,8 @@
+import { setAuth } from './auth.js';
+
+//helpers
+funtion $(sel) { return document.querySelector(sel); }
+function first(...els) { return else.find(Boolean); }
+
+//constants
+const 
