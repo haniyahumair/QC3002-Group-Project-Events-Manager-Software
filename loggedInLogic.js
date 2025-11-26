248846f2da34c4b01d4a5d104f3dfce242b44d7f
Haniyah Umair <haniyah.umair.18@gmail.com>
2025-11-26 09:28:28 +0300

    Updated js logic

--- loggedInLogic.js
+++ loggedInLogic.js
@@ -1,44 +1,59 @@
+let isLoggedIn = true;
+
 function checkLoginStatus() {
-    const isLoggedIn = true;
 
     const profileIcon = document.getElementById('profileIcon');
     const cartIcon = document.getElementById('cartIcon');
     const loginBtn = document.getElementById('loginBtn');
-    const viewDetailsBtn = document.querySelectorAll('.viewDetailBtn')
-    const createEventBtn = document.querySelector('.createEvent')
+    const signOutBtn = document.getElementById('signOutBtn');
 
     if (isLoggedIn) {
-        profileIcon.style.display = 'inline-flex';
-        cartIcon.style.display = 'inline-flex';
-
-        loginBtn.style.display = 'none';
+        if(profileIcon) profileIcon.style.display = 'inline-block';
+        if(cartIcon) cartIcon.style.display = 'inline-block';
+        if(loginBtn) loginBtn.style.display = 'none';
+        if(signOutBtn) signOutBtn.style.display = 'inline-block'
     } else {
-        profileIcon.style.display = 'none';
-        cartIcon.style.display = 'none';
-
-        loginBtn.style.display = 'inline-block';
+       if(profileIcon) profileIcon.style.display = 'none';
+        if(cartIcon) cartIcon.style.display = 'none';
+        if(signOutBtn) signOutBtn.style.display = 'none';
+        if(loginBtn) loginBtn.style.display = 'inline-block';
     }
 
-    viewDetailsBtn.forEach(btn =>{
-        btn.style.cursor = 'pointer'
-        btn.addEventListener('click', () => {
-            if (isLoggedIn){
-                window.location.href = 'details.html'
-            }
-            else{
-                window.location.href = 'login.html'
-            }
-        })
-    })
+    const viewDetailsBtn = document.querySelectorAll('.viewDetailBtn');
+    const createEventBtn = document.querySelectorAll('.createEvent');
 
-    createEventBtn.addEventListener('click', () => {
-        if (isLoggedIn){
-            window.location.href = 'create.html'
-        }
-        else{
-            window.location.href = 'login.html'
-        }
-    })
+    
+    if(viewDetailsBtn.length > 0){
+        viewDetailsBtn.forEach(btn => { 
+            btn.style.cursor = 'pointer';
+            const eventId = btn.dataset.id;
+            btn.addEventListener('click', () => {
+                if (isLoggedIn){
+                    window.location.href = `details.html?id=${eventId}`;
+                }
+                else{
+                    window.location.href = 'login.html';
+                }
+            });
+        });
+    }
+
+
+    
+    if(createEventBtn.length > 0){
+        createEventBtn.forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                e.preventDefault();
+    
+                if (isLoggedIn){
+                     window.location.href = 'create.html' ;
+                } 
+                else{ 
+                    window.location.href = 'login.html' ;
+                }
+            });
+        });
+    }
 }
 
 // Run when DOM is ready
